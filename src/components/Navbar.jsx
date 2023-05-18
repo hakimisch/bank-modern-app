be23f1ce3970b409477f59bf37939caa45a8396b
hakimisch <hakimizaimi@gmail.com>
2023-05-18 15:31:16 +0800

    fixed navbar menu bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
             <div onClick={handleNav} className='block md:hidden'>
                 {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
             </div>
-            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[rgb(47,52,68)] ease-in-out duration-500'  : 'fixed left-[-100%]' }>
+            <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[rgb(47,52,68)] ease-in-out duration-500'  : 'fixed left-[-100%]' }>
                 <h1 className='w-full text-3xl font-bold text-[cyan] m-4'>REACT.</h1>
                 <ul className='uppercase p-4'>
                     <li className='p-4 border-b border-gray-400'>Home</li>
